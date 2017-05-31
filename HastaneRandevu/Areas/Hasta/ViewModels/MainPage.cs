ba61a071f3cf60dec1f42ba32b6f34d3f374cabf
ISMAILOUASUS\isma_ <ismailou.sa@gmail.com>
2017-05-31 16:02:36 +0300

    Home area done (Alaa)

---
+++ HastaneRandevu/Areas/Hasta/ViewModels/MainPage.cs
@@ -0,0 +1,28 @@
+﻿using HastaneRandevu.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HastaneRandevu.Areas.Hasta.ViewModels
+{
+    public class MainPage
+    {
+        internal List<Il> il;
+        internal List<SelectListItem> hastaneler;
+
+        [Required]
+        public virtual List<SelectListItem> Iller { get; internal set; }
+        [Required]
+        public IList<SelectListItem> Ilceler { get; set; }
+        [Required]
+        public IList<SelectListItem> Hastaneler { get; set; }
+
+        [Required]
+        public IList<SelectListItem> Klinikler { get; set; }
+        [Required]
+        public IList<SelectListItem> Doktorlar { get; set; }
+    }
+}
