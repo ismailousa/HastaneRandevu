81dfeeeb13585e8e5acfea5f7e199f5ec6bf1476
seymauzan <seyma5tas@gmail.com>
2017-05-31 17:57:10 +0300

    commit yusuf

---
+++ Auth.cs
@@ -0,0 +1,43 @@
+﻿using NHibernate.Linq;
+using HastaneRandevu.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HastaneRandevu
+{
+    public static class Auth
+    {
+        private const string UserKey = "HastaneRandevu.Auth.Userkey";
+        public static User User
+        {
+            get
+            {
+                if (!HttpContext.Current.User.Identity.IsAuthenticated)
+                {
+                    return null;
+                }
+
+                var user = HttpContext.Current.Items["UserKey"] as User;
+
+                if (user == null)
+                {
+                    user = Database.Session.Query<User>().FirstOrDefault(p => p.KimlikNo == HttpContext.Current.User.Identity.Name);
+
+                }
+
+
+                if (user == null)
+                {
+                    return null;
+                }
+
+
+                HttpContext.Current.Items["UserKey"] = user;
+
+                return user;
+            }
+        }
+    }
+}
